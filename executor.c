b67d0819d0f813cc2fc634cbefbd0a7df38120b1
getDolla <ywang8@stuy.edu>
2016-11-20 11:25:31 -0500

    added comments

--- executor.c
+++ executor.c
@@ -4,20 +4,25 @@
 
 
 int main(int argc, char *argv[]){
-  char command[100];
+  char command[100]; //for command
+  
+  //for strsep
   char *p = command;
   char *args[10];
+    
   printf("What would you like to do? \t");
-  fgets(command, sizeof(command), stdin);
-  strtok(command, "\n");
+  fgets(command, sizeof(command), stdin);//gets input from stream
+  strtok(command, "\n");//gets rid of the \n at the end. will separate command into tokens if there is \n in the string
+    
   int i = 0;
-  printf("%s\n", command);
+  //printf("%s\n", command);//debug
   while(p){
-    args[i] = strsep(&p, " ");
-    printf("%s\n", args[i]);
+    args[i] = strsep(&p, " ");//gets series of arguments
+    //printf("%s\n", args[i]);//debug
     i++;
   }
-  args[i] = NULL;
+    
+  args[i] = NULL;//add terminating null just in case
   execvp(args[0], args);
-  return 1;
+  return 0;
 }
